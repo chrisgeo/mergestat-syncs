8d8c50180eff7aec4b94da834350ec91e7039681
Chris George <chrisgeo@users.noreply.github.com>
2025-12-25 04:55:20 -0800

    fix(deploy): Grafana requirements are wild (#69)
    * Grafana requirements are wild
    
    * Fix build
    
    * Again
    
    * Blackbox testing grafana's plugin build progress
    
    * ...
    
    * one more time...
    
    * one more time...
    
    * one one more time...
    
    * who knows...
    
    * Sha! Add sha.
    
    * Here for a long time, not a good time
    
    * Santa, I want this part to be easier
    
    * Seriously here for a long time
    
    * Never gonna give u up...
    
    * NPM version gonna version good
    
    * Update version to v0.1.1-beta.1 in package.json
    
    Signed-off-by: Chris George <chrisgeo@users.noreply.github.com>
    
    * Fix version format in package.json
    
    Signed-off-by: Chris George <chrisgeo@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Chris George <chrisgeo@users.noreply.github.com>
    Co-authored-by: Chris George <chris@noreply.noreply.nope>

--- grafana/plugins/dev-health-panels/.config/webpack/webpack.config.ts
+++ grafana/plugins/dev-health-panels/.config/webpack/webpack.config.ts
@@ -146,6 +146,7 @@ const config = async (env): Promise<Configuration> => {
           // To `compiler.options.output`
           { from: hasReadme() ? 'README.md' : '../README.md', to: '.', force: true },
           { from: 'plugin.json', to: '.' },
+          { from: '../package.json', to: '.' },
           { from: '../LICENSE', to: '.' },
           { from: '../CHANGELOG.md', to: '.', force: true },
           { from: '**/*.json', to: '.' }, // TODO<Add an error for checking the basic structure of the repo>
